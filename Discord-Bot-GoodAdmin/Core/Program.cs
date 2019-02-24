acfe25780a24351e3e26314f5be0be9cd5c4277b
xrayhunter <3002612+xrayhunter@users.noreply.github.com>
2019-02-24 09:43:06 -0800

    v1.0.1

---
+++ Discord-Bot-GoodAdmin/Core/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+
+namespace GoodAdmin.Core
+{
+    // Docs: https://discord.foxbot.me/docs/guides/getting_started/intro.html
+    public class Program
+    {
+        public static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private async Task MainAsync()
+        {
+        }
+    }
+}
