21e19b5493d1c29b816ec4c5d1cf470bfb368667
xrayhunter <3002612+xrayhunter@users.noreply.github.com>
2019-02-26 17:50:06 -0800

    V1.0.3-a Added Testing Module

---
+++ Module-Test/Class1.cs
@@ -0,0 +1,19 @@
+﻿using GoodAdmin_API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_Test
+{
+    public class Class1 : APIModule
+    {
+        // Sync
+        public Task Load()
+        {
+            Console.WriteLine("Loaded Test");
+            return Task.CompletedTask;
+        }
+    }
+}
